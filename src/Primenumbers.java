ae05fe82eb024faa6815c66d58c4666b0179fc19
NagendraRaja <41227540+NagendraRaja@users.noreply.github.com>
2019-06-25 16:10:43 +0530

    Add files via upload

---
+++ src/Primenumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Primenumbers {
+	public static void main(String[] args) {
+
+		int len;
+		Scanner sc = new Scanner(System.in);
+		len = sc.nextInt();
+
+		for (int n = 1; n < len; n++) {
+
+			int count = 0;
+
+			for (int i = 2; i < n; i++) {
+
+				if (n % i == 0) {
+					count++;
+					break;
+				}
+			}
+
+			if (count == 0 && n != 1) {
+				System.out.print(n + " ");
+			}
+		}
+	}
+
+}
